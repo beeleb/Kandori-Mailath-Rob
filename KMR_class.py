530fa0db771b451670ac7d4fbd418290e1751dec
beeleb <ontkys@gmail.com>
2014-07-08 16:09:00 +0900

    X_tの分布をプロットできるようにしました 2つ程度のメソッドでしか使われないインスタンス変数が多すぎる気がするのでクラスを分けたほうがいいかも……？

--- KMR_class.py
+++ KMR_class.py
@@ -11,25 +11,37 @@ t = 100000
 epsilon = 0.1
 payoff = [[[4,4],[0,3]],[[3,0],[2,2]]]
 """
-def set_pay(payoff):
-    if type(payoff[0][0]) == list or tuple and type(payoff[0][0][0]) == int:
-        one_payoff = np.transpose(np.transpose(payoff)[0])
-        print'OK.'
-    elif type(payoff[0][0]) == int:
-        one_payoff = payoff
-        print'OK.'
+def set_pay(pf):
+    global one_payoff
+    WDT='Wrong Data Type ! '
+    if type(pf) == int:
+        print WDT
+    elif type(pf[0]) ==int:
+        print WDT
     else:
-        print 'Wrong Data Type ! '
+        if type(pf[0][0]) == list or type(pf[0][0]) ==tuple:
+            one_payoff = np.transpose(np.transpose(pf)[0])
+            print'OK. one_payoff is'
+            print str(one_payoff)
+        elif type(pf[0][0]) == int:
+            one_payoff = pf
+            print'OK. one_payoff is'
+            print str(one_payoff)
+        else:
+            print WDT
+
 
 
 class KMR:
     def __init__(self,n,p,epsilon):
         self.one_pay = one_payoff
         self.epsi = epsilon
-        self.X = X
         self.n = n
+        self.X = 0
         self.p = p
-        self.xs = xs
+        self.xs = 0
+        self.x_0 = 0
+        self.x_ts = []
         
     def det_X(self):#遷移行列をつくる
         self.X = np.zeros((self.n+1,self.n+1))
@@ -58,16 +70,20 @@ class KMR:
                 self.X[k][k+1] = ((self.n-k)/self.n)*0.5
                 self.X[k][k] += ((self.n-k)/self.n)*0.5
             else:
-                self.X[k][k+1] = ((self.n-k)/self.n)*epsilon*0.5
+                self.X[k][k+1] = ((self.n-k)/self.n)*self.epsi*0.5
                 self.X[k][k] += ((self.n-k)/self.n)*(1-self.epsi*0.5)
         self.X[0][0] = 1-self.epsi*0.5
         self.X[0][1] = self.epsi*0.5
         self.X[self.n][self.n-1] = self.epsi*0.5
         self.X[self.n][self.n] = 1-self.epsi*0.5
     
+    def set_x0(self):
+        self.x_0 = np.random.binomial(self.n,self.p)  # determine X_0
+    
     def sim(self, t):
-        x_0 = np.random.binomial(self.n,self.p)  # determine X_0
-        self.xs = mc_sample_path(self.X,init=x_0,sample_size=t)
+        self.det_X()
+        self.set_x0()
+        self.xs = mc_sample_path(self.X,init=self.x_0,sample_size=t)
         
     def simplot(self,t):
         self.sim(t)
@@ -75,14 +91,27 @@ class KMR:
         plt.legend()
         plt.show()
         
-
+    def hist(self,t,times): 
+        self.x_ts = []
+        self.det_X()
+        self.set_x0()
+        for i in range(times):
+            self.sim(t)
+            self.x_ts.append(self.xs[-1])
+            
+    def histplot(self,t,times):
+        self.hist(t,times)
+        ax = plt.subplot(111)
+        ax.hist(self.x_ts, alpha=0.6, bins=10)
+        plt.show()
 
 
 """
 入力の例
-payoff = [[4,0],[3,2]]
+payoff = [[[4,4],[0,3]],[[3,0],[2,2]]]
 set_pay(payoff)
 f = KMR(10,1/3,0.1)  # (人数,二項分布の確率,ε)
 f.det_X()
-f.simplot(100000)
+f.simplot(100000) # (時間の長さ)
+f.histplot(10000,100)  # (時間の長さ、回数)
 """
